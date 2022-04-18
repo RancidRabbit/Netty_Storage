238fd36ca8a9e673a8e4c856b11377fb32ebbee1
Andrew <asumerkin@bk.ru>
2022-04-18 14:14:51 +0300

    Initial commit

---
+++ network-storage-template-master/client/src/main/java/ru/gb/storage/client/Client.java
@@ -0,0 +1,10 @@
+package ru.gb.storage.client;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ network-storage-template-master/server/src/main/java/ru/gb/storage/server/Server.java
@@ -0,0 +1,11 @@
+package ru.gb.storage.server;
+
+
+public class Server {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
