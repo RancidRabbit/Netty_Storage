b0fa1a4e4f4688702ae3cf81d611e22ce4679724
Andrew <asumerkin@bk.ru>
2022-04-18 14:37:17 +0300

    Homework 2

--- network-storage-template-master/server/src/main/java/ru/gb/storage/server/Server.java
+++ network-storage-template-master/server/src/main/java/ru/gb/storage/server/Server.java
@@ -1,11 +1,75 @@
 package ru.gb.storage.server;
 
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
 public class Server {
 
-    public static void main(String[] args) {
+    private final int port;
 
-
+    public static void main(String[] args) throws InterruptedException {
+        new Server(9000).start();
     }
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        //ThreadPool отвечающий за инициализацию новых подключений
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        //ThreadPool обслуживающий всех активных клиентов
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) //Используем серверную версию сокета
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) {
+                            final ByteBuf regex = Unpooled.copiedBuffer("\n".getBytes());
+                            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,regex));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                    String body = (String) msg;
+                                    body = "[Server]: " + body + "\n";
+                                    final ByteBuf byteBuf = Unpooled.copiedBuffer(body.getBytes());
+                                    ctx.writeAndFlush(byteBuf);
+                                }
+
+                                @Override
+                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                    System.out.println("Cause exception");
+                                    cause.printStackTrace();
+                                    ctx.close();
+                                }
+
+                            });
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            Channel channel = server.bind(port).sync().channel();
+
+            System.out.println("Server started");
+            channel.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
 
 }
