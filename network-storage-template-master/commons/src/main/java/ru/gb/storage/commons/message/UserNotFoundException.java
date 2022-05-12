382def29530ee1b2cd48f80d42c519eb46a15da2
Andrew <98696546+RancidRabbit@users.noreply.github.com>
2022-05-12 19:36:22 +0300

    Delete UserNotFoundException.java

--- network-storage-template-master/commons/src/main/java/ru/gb/storage/commons/message/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.storage.commons.message;
-
-public class UserNotFoundException extends Throwable{
-    public static void main(String[] args) {
-
-        System.out.println("User not found!");
-    }
-}
